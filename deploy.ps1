b01d80dbc546bb7ce38bfc080052c4b52356b1ea
Peegram <peegram.society.inc@gmail.com>
2026-06-19 22:13:32 +0100

    deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,18 +1,17 @@
 # pOS Infrastructure: Automated Kali Linux VM Provisioner
-Clear-Host
-$TargetDir = "C:\pOS_Infrastructure"
-$DownloadPath = "$TargetDir\kali-linux-2026.1-virtualbox-amd64.7z"
+# Developed by Peegram
 
+Clear-Host
 Write-Host "====================================================" -ForegroundColor Cyan
 Write-Host "         INITIALIZING DOWNLOAD PIPELINE             " -ForegroundColor Cyan
 Write-Host "====================================================" -ForegroundColor Cyan
 
-if (!(Test-Path -Path $TargetDir)) {
-    New-Item -Path $TargetDir -ItemType Directory | Out-Null
-}
+# Force create the directory infrastructure
+New-Item -Path "C:\pOS_Infrastructure" -ItemType Directory -Force | Out-Null
 
-$KaliUrl = "https://cdimage.kali.org/kali-2026.1/kali-linux-2026.1-virtualbox-amd64.7z"
-Invoke-WebRequest -Uri $KaliUrl -OutFile $DownloadPath -UserAgent "Mozilla/5.0"
+# Securely stream the official production archive
+Invoke-WebRequest -Uri "https://cdimage.kali.org/kali-2026.1/kali-linux-2026.1-virtualbox-amd64.7z" -OutFile "C:\pOS_Infrastructure\kali-linux-2026.1-virtualbox-amd64.7z" -UserAgent "Mozilla/5.0"
 
-Write-Host "[✓] PIPELINE STAGE 1 COMPLETED SUCCESSFUL" -ForegroundColor Green
-Write-Host "[*] File stored at: C:\pOS_Infrastructure" -ForegroundColor Green
+Write-Host ""
+Write-Host "[✓] PIPELINE STAGE 1 COMPLETED SUCCESSFULLY" -ForegroundColor Green
+Write-Host "[*] Target Core Saved to C:\pOS_Infrastructure" -ForegroundColor Green
